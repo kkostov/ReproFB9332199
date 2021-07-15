e5b9313712ca2f55acaf81e8b6fc97117e4ac9ae
Konstantin Kostov <konstantin@headbright.be>
2021-07-15 08:59:08 +0200

    Initial Commit

---
+++ Repro40/Repro40App.swift
@@ -0,0 +1,17 @@
+//
+//  Repro40App.swift
+//  Repro40
+//
+//  Created by Konstantin Kostov on 15/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct Repro40App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
